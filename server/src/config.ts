b9df868b30c31d49e2a0c98d014e4b0aff0f6031
pcouto <pcouto@mosano.eu>
2020-03-13 08:39:33 +0000

    server running

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ const { env } = process;
 const ENV = env.NODE_ENV || 'development';
 const API_PORT = env.API_PORT || 8080;
 
-const CONFIG = {
+const CONFIG: {ENV: String, API_PORT: string | number} = {
   ENV,
   API_PORT,
 };
